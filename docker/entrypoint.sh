184d309a5666fff0e19fd57603ed3e0d51c209ee
Yan Chirino <yanpier.chirino@gmail.com>
2021-08-26 19:25:13 -0500

    feat: start base project for erp

---
+++ docker/entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+if [ -v DB_PASSWORD_FILE ]; then
+    DB_PASSWORD="$(< $DB_PASSWORD_FILE)"
+fi
+
+# set the postgres database host, port, user and password according to the environment
+# and pass them as arguments to the odoo process if not present in the config file
+: ${DB_HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+: ${DB_PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${DB_USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${DB_PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+
+DB_ARGS=()
+function check_config() {
+    param="$1"
+    value="$2"
+    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
+        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
+    fi;
+    DB_ARGS+=("--${param}")
+    DB_ARGS+=("${value}")
+}
+
+envsubst < /etc/odoo/tmpl.conf > "$ODOO_RC"
+
+check_config "db_host" "$DB_HOST"
+check_config "db_port" "$DB_PORT"
+check_config "db_user" "$DB_USER"
+check_config "db_password" "$DB_PASSWORD"
+
+case "$1" in
+    -- | odoo)
+        shift
+        if [[ "$1" == "scaffold" ]] ; then
+            exec odoo "$@"
+        else
+            wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+            exec odoo "$@" "${DB_ARGS[@]}"
+        fi
+        ;;
+    -*)
+        wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+        exec odoo "$@" "${DB_ARGS[@]}"
+        ;;
+    *)
+        exec "$@"
+esac
+
+exit 1
