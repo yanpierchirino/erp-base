9021995e499e7e630e2db2512e2b531530a18de3
Yan Chirino <yanpier.chirino@gmail.com>
2021-12-10 14:39:55 -0500

    fix: odoo.conf params

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,28 +2,6 @@
 
 set -e
 
-if [ -v DB_PASSWORD_FILE ]; then
-    DB_PASSWORD="$(< $DB_PASSWORD_FILE)"
-fi
-
-# set the postgres database host, port, user and password according to the environment
-# and pass them as arguments to the odoo process if not present in the config file
-: ${DB_HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
-: ${DB_PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${DB_USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${DB_PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
-
-DB_ARGS=()
-function check_config() {
-    param="$1"
-    value="$2"
-    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
-        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
-    fi;
-    DB_ARGS+=("--${param}")
-    DB_ARGS+=("${value}")
-}
-
 # READ ADDONS
 if [ -d "/mnt/src-addons/*" ]; then
     for dir in /mnt/src-addons/*; do export ERP_ADDONS_PATH=$ERP_ADDONS_PATH",$dir"; done
@@ -47,12 +25,6 @@ compgen -A variable ERP_ | while read v; do
     var_name="$v";
     var=${var_name/ERP_/};
     echo "${var,,} = ${!v}" >> "$ODOO_RC"; done
-
-# SET DATABASE PARAMETERS
-check_config "db_host" "$DB_HOST"
-check_config "db_port" "$DB_PORT"
-check_config "db_user" "$DB_USER"
-check_config "db_password" "$DB_PASSWORD"
 
 case "$1" in
     -- | odoo)
