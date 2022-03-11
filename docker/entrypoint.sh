63668f70b49f0ed965dcefef7e5e27871ee40199
Yan Chirino <yanpier.chirino@gmail.com>
2022-03-10 22:36:15 -0500

    fix: update entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -19,12 +19,14 @@ if [ -d "/mnt/vendor-addons/odoo/ee/*" ]; then
     for dir in /mnt/vendor-addons/odoo/ee/*; do export ERP_ADDONS_PATH=$ERP_ADDONS_PATH",$dir"; done
 fi
 
+
 # MAKE ODOO.CONF FILE
-# envsubst < /etc/odoo/tmpl.conf > "$ODOO_RC"
+envsubst < /etc/odoo/tmpl.conf > "$ODOO_RC"
 compgen -A variable ERP_ | while read v; do
     var_name="$v";
     var=${var_name/ERP_/};
     echo "${var,,} = ${!v}" >> "$ODOO_RC"; done
+
 
 case "$1" in
     -- | odoo)
